61029eb8b9505e73c771019aadfab8f398179e2f
camah17 <camah17@hotmail.com>
2018-02-04 12:10:36 +0100

    Add files via upload
    Added new text for the rules and images.

--- js/states/Home.js
+++ js/states/Home.js
@@ -9,11 +9,11 @@ Elematix.HomeState = {
 			console.log("come here");
 			this.level = level;
 			this.titleMessage = 'Score: ' + score;
-			this.subtitleMessage = 'Great job, you cleared the game !!!\nPlay again to get better score.';
+			this.subtitleMessage = 'Great job, you cleared the game!!\nPlay again for a better score.';
 			this.instructionMessage = 'TOUCH TO PLAY AGAIN';
 		} else {
-			this.titleMessage = 'Elematix';
-			this.subtitleMessage = 'Fire, Water, Earth, Air\nWhich ones will combine ?';
+			this.titleMessage = 'Elematix\n\n';
+			this.subtitleMessage = 'Fire - Water - Earth - Air\n\nRULES\nCombine 2 elements to form an answer\nBUT\nThe elements have rules by nature\nWater puts out fire: Fire - Water\n\nSCORING\nThe game is timed, the quicker you answer - the higher you score\n\nBEWARE!\nIncorrect answers get -15 points!';
 			this.instructionMessage = 'TOUCH TO START';
 		}
 	},
@@ -30,13 +30,27 @@ Elematix.HomeState = {
 
 		// subtitle
 		var subtitleStyle = {font: '16px Arial', align: 'center'};
-		this.subtitleText1 = this.game.add.text(this.game.world.centerX, 180, this.subtitleMessage, subtitleStyle);
+		this.subtitleText1 = this.game.add.text(this.game.world.centerX, 270, this.subtitleMessage, subtitleStyle);
 		this.subtitleText1.anchor.setTo(0.5);
 
 		// instruction
 		var instructionStyle = {font: '24px Arial'};
 		this.instructionText = this.game.add.text(this.game.world.centerX, 700, this.instructionMessage, instructionStyle);
 		this.instructionText.anchor.setTo(0.5);
+
+		//add images
+		this.fire = this.game.add.sprite(this.game.world.centerX - 250, this.game.world.centerY + 100, 'fire');
+		this.fire.width = 100;
+		this.fire.height = 100;
+		this.water = this.game.add.sprite(this.game.world.centerX - 120, this.game.world.centerY + 100, 'water');
+		this.water.width = 100;
+		this.water.height = 100;
+		this.earth = this.game.add.sprite(this.game.world.centerX + 10, this.game.world.centerY + 100, 'earth');
+		this.earth.width = 100;
+		this.earth.height = 100;
+		this.air = this.game.add.sprite(this.game.world.centerX + 140, this.game.world.centerY + 100, 'air');
+		this.air.width = 100;
+		this.air.height = 100;
 
 		// go to game when click
 		game.input.onDown.add(this.startGame, this);
